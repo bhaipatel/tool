a93e29372ce9ef24ac7b2a6d8370a1e0bd3c4722
bhaipatel <89388156+bhaipatel@users.noreply.github.com>
2022-03-05 16:04:31 +0530

    Update enumsub.sh

--- enumsub.sh
+++ enumsub.sh
@@ -7,9 +7,8 @@ enum_sub(){
 
 curl -s "https://jldc.me/anubis/subdomains/"$domain | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" >> main.txt
 ~/go/bin/./assetfinder -subs-only $domain -silent | tee main.txt
-~/go/bin/./subfinder -d $domain|anew main >> main
-~/go/bin/./amass enum -d $domain -passive|  main >> main
-
-sort -u  main >> main
+~/go/bin/./subfinder -d $domain| tee main.txt >> main.txt
+~/go/bin/./amass enum -d $domain -passive| tee main.txt >> main.txt
+cat main.txt | sort -u | tee $2
 }
 enum_sub
